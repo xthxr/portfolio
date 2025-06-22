42a31b7c3443034f18a6399e678134587b9be870
Athar Akram <129141365+xthxr@users.noreply.github.com>
2025-06-22 19:26:59 +0530

    Update contact.jsx

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -1,1 +1,8 @@
+export default function Contact() {
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <h1 className="text-2xl text-accent font-bold">Contact page coming soon...</h1>
+    </div>
+  );
+}
 
